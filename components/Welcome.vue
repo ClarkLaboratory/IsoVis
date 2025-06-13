f78ebce778bc291fbac84de93f7b345284d9b21e
chingyinwan <chingyin@student.unimelb.edu.au>
2025-06-13 11:31:56 +1000

    Add data for our medRxiv preprint on neuropsychiatric risk genes in the human brain

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -93,6 +93,14 @@
 
         <p></p>
 
+        <b-link href="https://www.medrxiv.org/content/10.1101/2024.02.22.24303189v2" target="_blank"><p style="margin-bottom: 0px">De Paoli-Iseppi R <i>et al.</i> <i>Long-read sequencing reveals the RNA isoform repertoire of neuropsychiatric risk genes in human brain.</i> medRxiv. 2024.02.22.24303189. Pre-print available at: https://doi.org/10.1101/2024.02.22.24303189 </p></b-link>
+        <b-button variant="primary" @click="downloadFile('DePaoliIseppi_et_al_medRxiv_2024.zip')">.zip (122 kB) <b-icon-download aria-hidden="true"></b-icon-download></b-button>
+        <b-button variant="primary" @click="downloadFile('DePaoliIseppi_et_al_medRxiv_2024.7z')">.7z (111 kB) <b-icon-download aria-hidden="true"></b-icon-download></b-button>
+        <b-button variant="primary" @click="downloadFile('DePaoliIseppi_et_al_medRxiv_2024.tar.gz')">.tar.gz (120 kB) <b-icon-download aria-hidden="true"></b-icon-download></b-button>
+        <b-button variant="primary" @click="downloadFile('DePaoliIseppi_et_al_medRxiv_2024.tar.xz')">.tar.xz (111 kB) <b-icon-download aria-hidden="true"></b-icon-download></b-button>
+
+        <p></p>
+
         <p style="margin-bottom: 0px">Demo data</p>
         <b-button variant="primary" @click="downloadFile('demo_data.zip')">.zip (3.4 kB) <b-icon-download aria-hidden="true"></b-icon-download></b-button>
         <b-button variant="primary" @click="downloadFile('demo_data.7z')">.7z (2.3 kB) <b-icon-download aria-hidden="true"></b-icon-download></b-button>
